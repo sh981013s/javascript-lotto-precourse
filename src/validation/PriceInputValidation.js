9feff40fcefe96c853f08a1ca13a56ddffeeb623
sh981013s <sh981013s@gmail.com>
2022-12-16 14:47:44 +0900

    feat(priceInputValidation): 유저 구매구입 input 유효성 검사 로직 구현

--- src/validation/PriceInputValidation.js
+++ src/validation/PriceInputValidation.js
@@ -1,5 +1,11 @@
+const { USER_PRICE_MESSAGES } = require("../constants/Messages");
+
 class PriceInputValidation {
-  static valdiate() {}
+  static validate(userPriceInput) {
+    if (typeof userPriceInput !== "number" || userPriceInput % 1000) {
+      throw new Error(USER_PRICE_MESSAGES.ERROR);
+    }
+  }
 }
 
 module.exports = PriceInputValidation;
